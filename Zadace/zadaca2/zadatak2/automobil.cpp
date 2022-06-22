13c241685693b06f796f8434d835f88f51812446
vedad-e <107326832+vedad-e@users.noreply.github.com>
2022-06-22 18:07:22 +0200

    Delete Zadace/zadaca2/zadatak2 directory

--- Zadace/zadaca2/zadatak2/automobil.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "automobil.hpp"
-
-std::ostream& operator << (std::ostream& out, const Automobil& automobil){
-  return out << 
-    automobil.getModel() << " " <<
-    automobil.getBoja() << " " <<
-    automobil.getGodiste();
-}
